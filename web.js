d1fe37e0ee2ce77f01e11e9de83aae559f0681ef
Antti Mattila <antti.mattila@reaktor.fi>
2014-02-08 23:52:31 +0200

    Add index html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
-var express = require('express');
-var app = express();
-app.use(express.logger());
+var express = require('express')
+var app = express()
+app.use(express.logger())
+app.set('views', __dirname + '/')
+app.engine('html', require('ejs').renderFile)
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+app.get('/', function(req, res) {
+  res.render('index.html');
+})
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("Listening on " + port)
 });
