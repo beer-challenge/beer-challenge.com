f9712611bca6e6f375185ca664bacbc54d772d1d
Antti Mattila <antti.mattila@reaktor.fi>
2014-02-09 01:15:15 +0200

    Add logging for non WebGL browsers

--- web.js
+++ web.js
@@ -1,11 +1,17 @@
 var express = require('express')
 var app = express()
 app.use(express.logger())
+app.use(express.bodyParser())
 app.set('views', __dirname + '/')
 app.engine('html', require('ejs').renderFile)
 
 app.get('/', function(req, res) {
-  res.render('index.html');
+  res.render('index.html')
+})
+
+app.post('/noBeer', function(req, res) {
+  console.log('No beer', req.body)
+  res.end()
 })
 
 var port = process.env.PORT || 5000
