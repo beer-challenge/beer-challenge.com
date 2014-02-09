06025c4b6fad23f560172a1c2c2f310cea8281c4
Antti Mattila <antti.mattila@reaktor.fi>
2014-02-09 20:19:25 +0200

    Add email form

--- web.js
+++ web.js
@@ -14,7 +14,29 @@ app.post('/noBeer', function(req, res) {
   res.end()
 })
 
+var mongo = require('mongodb')
+var mongoUri = process.env.MONGOHQ_URL
+
+app.post('/contact', function(req, res) {
+  mongo.Db.connect(mongoUri, function(err, db) {
+    if (err)
+      res.send(530)
+    else
+      db.collection('contact', function(errr, collection) {
+        if (errr)
+          res.send(531)
+        else
+          collection.insert({email: req.body['email']}, {safe: true}, function(errrr, rs) {
+            if (errrr)
+              res.send(532)
+            else
+              res.send(204)
+          })
+      })
+  })
+})
+
 var port = process.env.PORT || 5000
 app.listen(port, function() {
   console.log("Listening on " + port)
-});
+})
