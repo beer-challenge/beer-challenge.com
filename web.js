de7b4f4488526bfe387a19e12dd7b3057ede16cd
Antti Mattila <antti.mattila@reaktor.fi>
2014-02-23 16:13:01 +0200

    Add highscores endpoint with dummy data

--- web.js
+++ web.js
@@ -14,6 +14,25 @@ app.get('/nexus', function(req, res) {
   res.download(file)
 })
 
+app.post('/highscores', function(req, res) {
+  console.log('Highscores', req.body)
+  res.send({
+    list: [
+      { name: 'yue', time: '1.1' },
+      { name: 'rgd', time: '1.2' },
+      { name: 'lop', time: '2.0' },
+      { name: 'xxx', time: '2.2' },
+      { name: 'lui', time: '3.1' },
+      { name: 'wer', time: '7.3' },
+      { name: 'bvn', time: '12.1' },
+      { name: 'iuo', time: '111.1' },
+      { name: 'iop', time: '123.0' },
+      { name: 'rte', time: '221.8' },
+    ],
+    type: '24h'
+  })
+})
+
 app.post('/noBeer', function(req, res) {
   console.log('No beer', req.body)
   res.end()
